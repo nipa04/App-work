49bcde39c98bfd5ba32c88f91177f26b72c316bd
nipa04 <nipa.farjana04@gmail.com>
2018-04-11 12:07:39 -0400

    Changing the UI

--- ColourMapper/ColourMapper/ViewController.swift
+++ ColourMapper/ColourMapper/ViewController.swift
@@ -11,25 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var redView: UIView!
-    
-    @IBOutlet weak var greenView: UIView!
-    
-    @IBOutlet weak var blueView: UIView!
-
-    @IBOutlet weak var yellowView: UIView!
-    
-    
-    @IBAction func colourMapped(_ sender: UIButton) {
-        if view == redView {
-            Colour.text = "Colour :RED"
-            UIView.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-
-        }
         
         
       
-    }
+   
     
     
 }
