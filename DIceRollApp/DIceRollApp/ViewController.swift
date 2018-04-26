d9b38638624beb5e6c68ef3d2face4071eee6779
nipa04 <nipa.farjana04@gmail.com>
2018-04-26 12:28:08 -0400

    commiting Dice Roll app

--- DIceRollApp/DIceRollApp/ViewController.swift
+++ DIceRollApp/DIceRollApp/ViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.swift
 //  DIceRollApp
 //
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let firstNumber = arc4random_uniform(6) + 1
 
+        let secondNumber = arc4random_uniform(6) + 1
+     
+        label.text = "The sum is: \(firstNumber + secondNumber)"
+        leftImageView.image = UIImage(named: "Dice\(firstNumber)")
+        rightImageView.image = UIImage(named: "Dice\(secondNumber)")
+        
+    }
+    
 }
 
