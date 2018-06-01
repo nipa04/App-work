d63df68471d24e494550d3a63730e300147a92b7
iOS developer <nipa.farjana04@gmail.com>
2018-06-01 10:22:01 -0400

    commiting

---
+++ autoLayoutPractice/autoLayoutPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoLayoutPractice
+//
+//  Created by iOS developer on 5/31/18.
+//  Copyright © 2018 iOS developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
