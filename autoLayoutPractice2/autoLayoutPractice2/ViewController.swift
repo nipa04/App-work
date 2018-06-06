8050ea3dc4109bbb81025c67c2e5b0fe9697de2c
iOS developer <nipa.farjana04@gmail.com>
2018-06-06 14:44:02 -0400

    layout commiting

---
+++ autoLayoutPractice2/autoLayoutPractice2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  autoLayoutPractice2
+//
+//  Created by iOS developer on 6/5/18.
+//  Copyright © 2018 iOS developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBAction func tappedButton(_ sender: UIButton) {
+        sender.removeFromSuperview()
+        
+    }
+    
+
+}
+
