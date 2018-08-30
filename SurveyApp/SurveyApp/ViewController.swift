cf1f555a1d82f870c8c5f0c1036cd64963cfae69
iOS developer <nipa.farjana04@gmail.com>
2018-08-30 13:02:20 -0400

    commit

---
+++ SurveyApp/SurveyApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SurveyApp
+//
+//  Created by iOS developer on 8/30/18.
+//  Copyright © 2018 iOS developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var surveyTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
